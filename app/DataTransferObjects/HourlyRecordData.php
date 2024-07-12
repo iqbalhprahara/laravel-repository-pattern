4575090fd5ab9861414b2a2be98722ca2df1a24f
Iqbal Hikmat Prahara <iqbalhprahara@gmail.com>
2024-07-12 21:02:50 +0700

    feat: add uuid to serialized hourly data

--- app/DataTransferObjects/HourlyRecordData.php
+++ app/DataTransferObjects/HourlyRecordData.php
@@ -4,17 +4,26 @@ namespace App\DataTransferObjects;
 
 use Illuminate\Support\Carbon;
 
-class HourlyRecordData
+readonly class HourlyRecordData
 {
     public function __construct(
-        public readonly Carbon $timestamp,
-        public readonly int $maleCount,
-        public readonly int $femaleCount,
+        public Carbon $timestamp,
+        public int $maleCount,
+        public int $femaleCount,
     ) {}
+
+    /**
+     * Generate uuid for the instance
+     */
+    protected static function generateUuid(): string
+    {
+        return (string) \Illuminate\Support\Str::orderedUuid();
+    }
 
     public function serialize()
     {
         return serialize([
+            'uuid' => static::generateUuid(),
             'timestamp' => $this->timestamp->timestamp,
             'male_count' => $this->maleCount,
             'female_count' => $this->femaleCount,
